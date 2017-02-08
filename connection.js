bda5c2c7d65682463a708260230497b0c455789c
Kirti3252 <kirti.irkal@vmokshagroup.com>
2017-02-08 18:17:23 +0530

    connection.js updated
    by kirti

--- connection.js
+++ connection.js
@@ -6,13 +6,14 @@ function Connection() {
   this.init = function() {
     this.pool = mysql.createPool({
       connectionLimit: 10,
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'testapp',
+      host: 'sql6.freesqldatabase.com',
+      user: 'sql6157867',
+      password: '5xytwqbMJE',
+      database: 'sql6157867',
       port:3306
     });
   };
+
  
   this.acquire = function(callback) {
     this.pool.getConnection(function(err, connection) {
