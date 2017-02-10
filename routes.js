b5da92fecc4d6972026b6a2b44f0923bafac2446
Kirti3252 <kirti.irkal@vmokshagroup.com>
2017-02-10 16:40:23 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -57,7 +57,7 @@ app.get('/showSignUpPage',function(req,res){
   res.sendFile('signup.html',{'root':__dirname + '/models'})
 });
 app.get('/getuser',function(req,res){
-  res.sendFile('https://nodejswithphp.000webhostapp.com/index.php')
+  res.sendFile('getuser.html',{'root':__dirname + '/models')
 });
 
 app.get('/message',function(req,res){
